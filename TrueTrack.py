1901f3355bf59aabfd6f7500f740685aa1767002
IsoPorksu <carrotpiegoo@gmail.com>
2025-01-22 11:02:19 +0200

    Various updates

--- TrueTrack.py
+++ TrueTrack.py
@@ -35,19 +35,19 @@ destinations = {
 
 def check_friends(filtered_vehicles):
     taken = []
-    for vehicle_number, [current, next, eta, track, dest, speed, dep] in filtered_vehicles.items():
-        for other_vehicle_number, [other_current, other_next, other_eta, other_track, other_dest, other_speed, other_dep] in filtered_vehicles.items():
+    for vehicle_number, [current, next, eta, track, dest, speed, dep, seq] in filtered_vehicles.items():
+        for other_vehicle_number, [other_current, other_next, other_eta, other_track, other_dest, other_speed, other_dep, other_seq] in filtered_vehicles.items():
             if other_vehicle_number != vehicle_number and (vehicle_number and other_vehicle_number) not in taken:
                 if track == other_track:
                     if (current == other_current or (current == other_next and next == "") or (next == other_current and other_next == "")) and dep == other_dep:
                         eta = 0 if eta == "" else int(eta)
                         other_eta = 0 if other_eta == "" else int(other_eta)
                         if eta < other_eta:
-                            vehicles[other_vehicle_number] = current, next, eta, track, dest, speed, other_dep
+                            vehicles[other_vehicle_number] = current, next, eta, track, dest, speed, dep, other_seq
                             friends[other_vehicle_number] = vehicle_number
                             friends[vehicle_number] = other_vehicle_number
                         else:
-                            vehicles[vehicle_number] = other_current, other_next, other_eta, other_track, other_dest, other_speed, dep
+                            vehicles[vehicle_number] = other_current, other_next, other_eta, other_track, other_dest, other_speed, other_dep, seq
                             friends[other_vehicle_number] = vehicle_number
                             friends[vehicle_number] = other_vehicle_number
                         taken.append(vehicle_number)
@@ -79,11 +79,16 @@ def check_timetable():
     timetable = {1: "P", 2: "T", 3: "T", 4: "T", 5: "P", 6: "L", 7: "S"}.get(date.isoweekday(), "")
     date = date.strftime("%d.%m.%y")
     timetable = next((item[1] for item in specials if item[0] == date), timetable)
+    #timetable = "HÄTÄ"
     return timetable
 
-def print_maker(car, station, next, eta, destination, speed, departure):
+def print_maker(car, station, next, eta, destination, speed, departure, seq):
+    if car < 299 and seq == 1: car = "^"+str(car)
+    else: car = " " + str(car)
     if car in (131, 320):
         car = str(car)+"*"
+    if car in (129, 169):
+        car = str(car)+","
     if car in (141, 179):
         car = str(car)+"'"
     if car in (000, 135):
@@ -91,23 +96,22 @@ def print_maker(car, station, next, eta, destination, speed, departure):
     if car in (000, 155):
         car = str(car)+"-"
     if next and eta:
-        print(f" {car:<4}| {station:>4} -> {next:<4}{eta:>4}s | {destination:<11}|", end="")
+        print(f"{car:<5}| {station:>4} -> {next:<4}{eta:>4}s | {destination:<11}|", end="")
     elif next:
-        print(f" {car:<4}| {station:>4} -> {next:<4}    | {destination:<11}|", end="")
+        print(f"{car:<5}| {station:>4} -> {next:<4}    | {destination:<11}|", end="")
     else:
-        print(f" {car:<4}| {station:>4}              | {destination:<11}|", end="")
-    if len(str(car))>3:
-        car = str(car)[:3]
-    car = int(car)
-    
+        print(f"{car:<5}| {station:>4}              | {destination:<11}|", end="")
+    car = int(str(car)[1:4])
     friend = friends.get(car, "")
+    if car < 299 and seq == 2: friend = "^"+str(friend)
+    else: friend = " " + str(friend)
     if speed not in ["0", 0] and next:
-        print(f" {friend:<4}| {speed:>2} | {departure}" if friend else f"     | {speed:>2} | {departure}")
+        print(f"{friend:<5}| {speed:>2} | {departure}" if friend else f"     | {speed:>2} | {departure}")
     else:
-        print(f" {friend:<4}|    | {departure}" if friend else f"     |    | {departure}")
+        print(f"{friend:<5}|    | {departure}" if friend else f"     |    | {departure}")
     
     track = station[-1] if station.endswith(("1", "2")) else ""
-    vehicles[car] = station, next, eta, track, destination, speed, departure
+    vehicles[car] = station, next, eta, track, destination, speed, departure, seq
 
 
 def print_vehicle_table():
@@ -123,7 +127,7 @@ def print_vehicle_table():
     moment = time.time()
     ping = str(ceil((moment - last_message) * 1000)) + "ms"
     print(f" Runtime: {runtime_seconds}  Ping: {ping:>5}  Time: {now.strftime('%H:%M:%S')}  Timetable: {check_timetable()}")
-    print(" Set |  Now -> Next  ETA | Destination|Set 2|Sped| Vuoro")
+    print(" Set |  Now -> Next  ETA | Destination|Set 2|Sped|Depart")
     print(" ----|-------------------|------------|-----|----|------")
     counters = {'vs': 0, 'mm': 0, 'tap': 0, 'kil': 0, 'm100_count': 0, 'm200_count': 0, 'm300_count': 0, 'o300_count': 0}
     destination_map = {"VS": 'vs', "  MM": 'mm', "    TAP": 'tap', "       KIL": 'kil'}
@@ -131,11 +135,11 @@ def print_vehicle_table():
         (range(201, 224), 'm200_count', 0.5),
         (range(301, 321), 'm300_count', 1),
         (range(321, 326), 'o300_count', 1)]
-    for vehicle_number, [station, next, eta, track, destination, speed, departure] in sorted_vehicles.items():
+    for vehicle_number, [station, next, eta, track, destination, speed, departure, seq] in sorted_vehicles.items():
         eta = "" if eta == "" else str(eta)
         if eta == "0":
             eta = ""
-        print_maker(vehicle_number, station, next, eta, destination, speed, departure)
+        print_maker(vehicle_number, station, next, eta, destination, speed, departure, seq)
         stock = 0.5 if int(str(vehicle_number)[:3]) < 300 else 1
         if next == "":
             station_counter =+1
@@ -150,18 +154,19 @@ def print_vehicle_table():
     p = float(counters['vs'] + counters['kil'])
     m2_count = f"{ceil(o)}xM2" if o.is_integer() else f"{o}xM2"
     m1_count = f"{ceil(p)}xM1" if p.is_integer() else f"{p}xM1"
-    print(f" {total:<4}| {m1_count:<7}   {m2_count:>7} | {ceil(counters['vs']):<2} {ceil(counters['mm']):<2} "
-          f"{ceil(counters['tap']):<2} {ceil(counters['kil']):<2}|     |    |")
+    print(f" {ceil(counters['m100_count'])+ceil(counters['m200_count'])+ceil(counters['m300_count'])+ceil(counters['o300_count']):<4}| {m1_count:<7}   {m2_count:>7} | {ceil(counters['vs']):<2} {ceil(counters['mm'])}  {ceil(counters['tap'])} {ceil(counters['kil']):<2} |     |    |")
     emotions = [":D", ":)", ":|", ":(", ":C", ">:("]
     emotion = emotions[counters['o300_count']]
     print(f" {ceil(counters['m100_count'])}xM100, {ceil(counters['m200_count'])}xM200, {ceil(counters['m300_count'])}xM300, {ceil(counters['o300_count'])}xO300 = {emotion}")
-    if station_counter >= 20:
-          print("ALL TRAFFIC IS STOPPED.") 
+    if int(station_counter) > int(o+p-2) and int(runtime.total_seconds())>1:
+        print(" ALL TRAFFIC IS STOPPED")
+    #print(" NO TRAFFIC LAS-KP")
 
 
 def update_vehicle_table():
     while True:
         timetable = check_timetable()
+        #timetable = "HÄTÄ" 
         print_vehicle_table()
         time.sleep(1)
 
@@ -171,7 +176,7 @@ def on_message(client, userdata, message):
     data = json.loads(message.payload.decode())
     vehicle_number = data.get('VP', {}).get('veh')
     latitude, longitude = data.get('VP', {}).get('lat'), data.get('VP', {}).get('long')
-    line, day, dep = data.get('VP', {}).get('desi'), data.get('VP', {}).get('oday'), data.get('VP', {}).get('start')
+    line, day, dep, seq = data.get('VP', {}).get('desi'), data.get('VP', {}).get('oday'), data.get('VP', {}).get('start'), data.get('VP', {}).get('seq') 
     speed = str(ceil(data.get('VP', {}).get('spd', 62.5) * 3.6))
 
     if (latitude, longitude) in coordinates:
@@ -181,7 +186,6 @@ def on_message(client, userdata, message):
         elif next == "Post-TAPx1": next, pos = ("URP" if line == "M1" else "TAPG" if line == "M2" else "", "84" if line == "M1" else "64" if line == "M2" else "")
         elif next == "Post-TAPx2": next, pos = ("URP" if line == "M1" else "TAPG" if line == "M2" else "", "79" if line == "M1" else "58" if line == "M2" else "")
         elif current == "Pre-TAP": current = "URP" if line == "M1" else "TAPG" if line == "M2" else ""
-
         eta = eta_maker(pos)
         last_etas[vehicle_number] = eta
         day = findDayNumber(day)
@@ -203,17 +207,25 @@ def on_message(client, userdata, message):
             if key in vuoros:
                 dep = vuoros[key]
   
+ 
         if current not in ["KILK", "TAPG", "SVV", "VSG", "MMG", ""]: current += track
+        """if current in ["KILK", "KIL1" , "ESL1", "SOU1", "KAI1", "FIN1", "MAK1", "NIK1", "URP1", "TAP1", "OTA1", "KEN1", "KOS1", "LAS1"]: destination = "    LAS"
+        if current in ["KIL2", "ESL2", "SOU2", "KAI2", "FIN2", "MAK2", "NIK2", "URP2", "TAP2", "OTA2", "KEN2", "KOS2", "LAS2"]: destination == "       KIL"
+        if current in ["MMG", "MM2", "KL2", "MP2", "IK2", "VSG", "VS2", "ST2", "HN2", "KS2", "KA2", "SN2", "HT2", "HY2", "RT2", "KP2", "KPG"]: destination = "     KP" """
         if next not in ["KILK", "TAPG", "SVV", "VSG", "MMG", ""]: next += track
         if next == "VS1" and int(eta) < 60 and int(speed) < 36:
             next == "VS2"
         departure = dep
+        
+        
         if len(current) == 2: current = " " + current
         speed = min(max(int(speed), 15), 81) if int(speed) != 0 else 0
-        vehicles[vehicle_number] = current, next, eta, track, destination, speed, departure
-        if vehicle_number == 203: vehicles[219] = current, next, eta, track, destination, speed, departure
+        vehicles[vehicle_number] = current, next, eta, track, destination, speed, departure, seq
+        
+        if vehicle_number == 203: vehicles[219] = current, next, eta, track, destination, speed, departure, seq
 
 timetable = check_timetable()
+#timetable = "HÄTÄ"
 client = mqtt.Client()
 client.on_message = on_message
 client.connect(broker, port)
