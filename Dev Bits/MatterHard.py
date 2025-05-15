4406ba62e3fafa4386373a255258aa2eec228dc3
IsoPorksu <carrotpiegoo@gmail.com>
2025-05-15 20:02:08 +0300

    Extra co-ords

--- Dev Bits/MatterHard.py
+++ Dev Bits/MatterHard.py
@@ -9,7 +9,7 @@ coordinates = []
 def on_message(client, userdata, message):
     data = json.loads(message.payload.decode())
     vehicle_id = data.get('VP', {}).get('veh', 'Unknown')
-    if vehicle_id == 320: # ALWAYS CHECK VEHICLE NUMBER!
+    if vehicle_id == 324: # ALWAYS CHECK VEHICLE NUMBER!
         latitude = data.get('VP', {}).get('lat', 'Unknown')
         longitude = data.get('VP', {}).get('long', 'Unknown') 
         #acceleration = data.get('VP', {}).get('acc', 'Unknown')
@@ -18,7 +18,7 @@ def on_message(client, userdata, message):
         newCoordinate = [latitude, longitude]
         coordinates.append(newCoordinate)
     formatString = "  [[{lat},{long}], [\"\",\"\",\"1\",\"\"]],\n" #CHECK TRACK NUMBER!
-    with open("Dev Bits/spare_coords/metro_coords_MP2IK2.json", "w") as outfile:
+    with open("Dev Bits/spare_coords/metro_coords_PT2IK2.json", "w") as outfile:
         outfile.write("[\n")
         for coordinate in coordinates:
             outfile.write(str.format(formatString, lat=coordinate[0], long=coordinate[1]))
